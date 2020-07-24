c60c6f1deaad070153573e953cc626f9f9e16873
Edson <grubhart@gmail.com>
2020-07-24 17:18:07 -0500

    updated infra names

--- server.js
+++ server.js
@@ -8,7 +8,7 @@ const PORT = process.env.PORT || 3000;
 // App
 const app = express();
 app.get('/', function (req, res) {
-    res.send('Hello UCIC\n');
+    res.send('Hello kath\n');
 });
 
 app.listen(PORT);
